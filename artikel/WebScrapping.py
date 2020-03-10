0134e0517a12ddf3add4937a75bee36387f410dd
Fachri Muhammad Bandang Haq <60908604+fachribandang@users.noreply.github.com>
2020-03-10 22:44:35 +0700

    edit artikel

--- artikel/WebScrapping.py
+++ artikel/WebScrapping.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 from bs4 import BeautifulSoup
 
 
@@ -9,17 +10,21 @@ page = requests.get("https://www.republika.co.id/")
 obj = BeautifulSoup(page.text, 'html.parser')
 all = []
 for headline in obj.find_all('div',class_='teaser_conten1'):
+    x = json.dumps(headline.find('h1').text)
     y = json.dumps(headline.find('h2').text)
     z = json.dumps(headline.find('div', class_='date').text)
+    u = json.dumps(time.time())
+    my_dict['Kategori']=x
     my_dict['Judul']=y
-    my_dict['Waktu']=z    
+    my_dict['Waktu']=z   
+    my_dict['pengambilan']=u
     
     all.append( dict(my_dict))    
     
 
 dict_web = all
 with open('scrap.json', 'w') as file:
-    json.dump(dict_web, file, indent=3)
+    json.dump(dict_web, file, indent=5)
 
 
    
