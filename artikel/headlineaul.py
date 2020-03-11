881860443144310276f1fbd5d6fd9a1b7b7389b9
arahmi6 <57357243+arahmi6@users.noreply.github.com>
2020-03-11 11:51:33 +0700

    Add files via upload

---
+++ artikel/headlineaul.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import datetime
+from bs4 import BeautifulSoup
+
+mydict = {}
+
+currentDT = datetime.datetime.now()
+page = requests.get("https://www.republika.co.id/")
+obj = BeautifulSoup(page.text, 'html.parser');
+all = []
+
+for headline in obj.find_all('div',class_='teaser_conten1'):
+	x = json.dumps(headline.find('h1').text)
+	y = json.dumps(headline.find('h2').text)
+	z = json.dumps(headline.find('div', class_='date').text)
+	mydict['Kategori']=x
+	mydict['Judul']=y
+	mydict['Waktu']=z
+	mydict['Waktu_skrg']=currentDT.strftime("%d %b %Y %H:%M:%S")
+
+	all.append( dict(mydict))
+dictweb = all
+print(mydict)
+with open('headlineaul.json', 'w') as file:
+	json.dump(dictweb, file, indent=4)
